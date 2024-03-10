2a6ead3a3c8d89c0077e2073148aa7454b57b075
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-03-11 00:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function calculateGrade(percentage) {
+function calculateGrade(p) {
   //your code here
+	let ans="";
+if(p>90){
+	ans='A'
+}else if(p>=80 && p<90){
+	ans='B'
+}else if(p>=70 && p<80){
+	ans='C'
+}else if(p>=60 && p<70){
+	ans='D'
+}else{
+	ans='F'
+}
+
+
+return ans;
+	
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
